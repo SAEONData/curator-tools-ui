117c7fdb3183fa88fc898a56eac8984467fee39b
dcugh <dianauys@gmail.com>
2020-06-29 09:02:47 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+import * as ReactDom from 'react-dom'
+import { Route, BrowserRouter as Router, Link, Switch } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+import Home from './components/Home'
+import Auth from './components/Auth'
+import Records from './components/Records'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <Router>
+            <div> 
+                <nav className='navbar navbar-expand navbar-dark bg-dark'>
+                <a href='/' className='navbar-brand'>
+                    curator
+                </a>
+                <div className='navbar-nav mr-auto'>
+                    <li className='nav-item'>
+                    <Link to={'/'} className='nav-link'>
+                        home
+                    </Link>
+                    </li>
+                    <li className='nav-item'>
+                    <Link to={'/records'} className='nav-link'>
+                        records
+                    </Link>
+                    </li>
+                </div>
+                </nav>
 
-export default App;
+                <div className='container mt-3'>
+                <Switch>
+                    <Route exact path={['', '/home']} component={Home} />
+                    <Route exact path='/records' component={Records} />
+                </Switch>
+                </div>
+            </div>
+        </Router>
+    )
+}
+    
+export default App
+
+ 
+    
+
+
